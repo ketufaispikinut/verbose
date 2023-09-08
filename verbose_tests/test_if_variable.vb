554b698b517922bca9f4b15e47117a6de435d308
ktfpkn <lavoieleon@gmail.com>
2023-09-08 17:43:47 -0400

    src/ verbose_tests/test_clock_100_000.vb verbose_tests/test_erreur.vb

---
+++ verbose_tests/test_if_variable.vb
@@ -0,0 +1,13 @@
+
+
+//Ne compile pas
+Gnorf vaut 17
+
+Si Gnorf est égal à 17 alors
+    DixSept vaut 5
+   // Afficher "DixSept: "+DixSept
+  //  Afficher "Douze"
+    #Afficher "DixSept + "+ DixSept
+fin
+Afficher DixSept
+
